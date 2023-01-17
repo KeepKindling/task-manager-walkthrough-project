df5828e32a0a435cbc3eb7bf8351181c56b553f8
KeepKindling <flashywizard127@gmail.com>
2023-01-17 11:02:42 +0000

    data is being submitted to database

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,7 +5,7 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)  # noqa
 
     def __repr__(self):
         # __repr__ to represent itself in thre form of a string
